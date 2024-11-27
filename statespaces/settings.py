e811649259f4a5e17017b1828e3232f606492459
Arthur Lluisma <ajclluisma@gmail.com>
2024-11-27 19:28:21 +0800

    Add base.html and index.html with basic styles

--- statespaces/settings.py
+++ statespaces/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'statespaces.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
 STATIC_URL = 'static/'
 
 # Default primary key field type
--- website/views.py
+++ website/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    ctx = {
+        "tests": [
+            "Test 1",
+            "Test 2",
+            "Test 3"
+        ]
+    }
+    return render(request, "website/index.html", ctx)
