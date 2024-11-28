01d319186ba3c1482d648becfd90564aaf3028dc
Arthur Lluisma <ajclluisma@gmail.com>
2024-11-29 00:09:37 +0800

    Fix filter javascript formatting

--- website/static/website/js/filter.js
+++ website/static/website/js/filter.js
@@ -1,20 +1,20 @@
-let filterState = false
+let filterState = false;
 
-let filter = document.querySelector(".filter")
-let filterHeading = document.querySelector(".filter__heading")
-let filterImg = document.querySelector(".filter__img")
-let filterTags = document.querySelector(".filter__tags")
+let filter = document.querySelector(".filter");
+let filterHeading = document.querySelector(".filter__heading");
+let filterImg = document.querySelector(".filter__img");
+let filterTags = document.querySelector(".filter__tags");
 
 filterHeading.addEventListener("click", (evt) => {
   if (!filterState) {
-    filterState = true
+    filterState = true;
     filter.style.marginBottom = "2.1rem";
-    filterImg.src = "../../../../media/images/arrow-up.svg"
+    filterImg.src = "../../../../media/images/arrow-up.svg";
     filterTags.style.display = "flex";
   } else {
-    filterState = false
+    filterState = false;
     filter.style.marginBottom = "0";
-    filterImg.src = "../../../../media/images/arrow-down.svg"
+    filterImg.src = "../../../../media/images/arrow-down.svg";
     filterTags.style.display = "none";
   }
-})
+});
