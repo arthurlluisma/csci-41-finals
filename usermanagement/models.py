0430d6e314c32f9011f6002fe640df8a49af3ca4
Chesca <fdjr401@gmail.com>
2024-12-02 21:06:25 +0800

    Fix models

--- usermanagement/models.py
+++ usermanagement/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True) 
     customer_id = models.BigAutoField(primary_key=True)  # primary key
     customer_first_name = models.CharField(max_length=255)
     customer_middle_initial = models.CharField(max_length=255)
@@ -13,7 +13,7 @@ class Customer(models.Model):
     customer_location = models.CharField(max_length=255)
 
     def __str__(self):
-        return "{}, {} {}.".format(
+        return "{}, {} {}".format(
             self.customer_last_name,
             self.customer_first_name,
             self.customer_middle_initial,
